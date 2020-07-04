4cd58ba144de74ea7d8e12a1a5e0f22ce365d04f
cantanert <cantanert@gmail.com>
2020-07-04 18:52:24 +0300

    product price and shipment info component UI mocked via hardcoded datas

--- src/components/pdp/product-price-and-shipment-info/ProductPriceAndShipmentInfo.js
+++ src/components/pdp/product-price-and-shipment-info/ProductPriceAndShipmentInfo.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {FiTruck} from "react-icons/fi"
 
 const ProductPriceAndShipmentInfo = () => {
   return (
       <div className="product-price-and-shipment-info">
-
+          <div className="product-total-price-title">
+              <p>Toplam</p>
+          </div>
+          <div className="product-price-and-shipment">
+              <p className="price">300.000,00 TL</p>
+              <div className="shipment">
+                  <p><span className="truck-icon"><FiTruck/></span> Kargo Ücreti: <span className="shipment-type">Alıcı Öder</span></p>
+              </div>
+          </div>
       </div>
   )
 };
