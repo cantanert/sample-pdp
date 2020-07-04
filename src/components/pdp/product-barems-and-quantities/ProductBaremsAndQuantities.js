2443bb465de37270d2e4fbaa6229f3b6c7aa3797
cantanert <cantanert@gmail.com>
2020-07-04 17:39:10 +0300

    product barems and quantites component UI mocked via hardcoded datas

--- src/components/pdp/product-barems-and-quantities/ProductBaremsAndQuantities.js
+++ src/components/pdp/product-barems-and-quantities/ProductBaremsAndQuantities.js
@@ -3,7 +3,56 @@ import React from "react";
 const ProductBaremsAndQuantities = () => {
     return (
         <div className="product-barems-and-quantity">
-
+            <div className="product-barems">
+                <div className="barem-prices-title">
+                    <p>Toptan Fiyat</p>
+                    <p>(Adet)</p>
+                </div>
+                <div className="barem-prices-wrapper">
+                    <div className="barem-price">
+                        <p>100 - 199</p>
+                        <p>1300 TL</p>
+                    </div>
+                    <div className="barem-price selected">
+                        <p>200 - 299</p>
+                        <p>880 TL</p>
+                    </div>
+                    <div className="barem-price">
+                        <p>300 - 399</p>
+                        <p>550 TL</p>
+                    </div>
+                    <div className="barem-price">
+                        <p>300 - 399</p>
+                        <p>550 TL</p>
+                    </div>
+                    <div className="barem-price">
+                        <p>300 - 399</p>
+                        <p>550 TL</p>
+                    </div>
+                    <div className="barem-price">
+                        <p>300 - 399</p>
+                        <p>550 TL</p>
+                    </div>
+                    <div className="barem-price">
+                        <p>300 - 399</p>
+                        <p>550 TL</p>
+                    </div>
+                    <div className="barem-price">
+                        <p>300 - 399</p>
+                        <p>550 TL</p>
+                    </div>
+                </div>
+            </div>
+            <div className="product-quantity">
+                <div className="product-quantity-title">
+                    <p>Adet</p>
+                </div>
+                <div className="product-quantity-input">
+                    <input type="text"/>
+                    <span>Adet</span>
+                    <span className="stock-quantity">Stok Adedi : <strong>600</strong></span>
+                </div>
+            </div>
         </div>
     )
 };
