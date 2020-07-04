88e263e376457fbba9c447289357d20324655390
cantanert <cantanert@gmail.com>
2020-07-04 16:16:16 +0300

    product min max info component UI mocked via hardcoded datas

--- src/components/pdp/product-min-max-informations/ProductMinMaxInformations.js
+++ src/components/pdp/product-min-max-informations/ProductMinMaxInformations.js
@@ -3,7 +3,8 @@ import React from "react";
 const ProductMinMaxInformations = () => {
     return (
         <div className="product-min-max-infos">
-            
+            <p className="price-per-piece"><span>3.000,00 TL - 3.300,00 TL</span> / Adet</p>
+            <p className="min-quantity">100 Adet (Minimum Sipariş Adedi)</p>
         </div>
     )
 };
