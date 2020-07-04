2190315421b383fe6634a31c66b3a339096b837c
cantanert <cantanert@gmail.com>
2020-07-04 16:43:45 +0300

    product selectable attributes component UI mocked via hardcoded datas

--- src/components/pdp/product-selectable-attributes/ProductSelectableAttributes.js
+++ src/components/pdp/product-selectable-attributes/ProductSelectableAttributes.js
@@ -3,7 +3,41 @@ import React from "react";
 const ProductSelectableAttributes = () => {
     return (
         <div className="product-selectable-attributes">
+            <div className="selectable-attribute">
+                <div className="attribute-name">
+                    <span>Hafıza</span>
+                </div>
+                <div className="attribute-options">
+                    <div className="option selected">
+                        <p>64 GB</p>
+                        <p>Ücretsiz</p>
+                    </div>
+                    <div className="option">
+                        <p>128 GB</p>
+                        <p>Ücretsiz</p>
+                    </div>
+                    <div className="option">
+                        <p>256 GB</p>
+                    </div>
+                </div>
+            </div>
+            <div className="selectable-attribute">
+                <div className="attribute-name">
+                    <span>Renk</span>
+                </div>
+                <div className="attribute-options">
+                    <div className="option">
+                        <p>Rose Gold</p>
+                    </div>
+                    <div className="option selected">
+                        <p>Black</p>
 
+                    </div>
+                    <div className="option">
+                        <p>Silver</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
