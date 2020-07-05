64f80eda9a2a77f38d29536b73b4bcfdcae97611
cantanert <cantanert@gmail.com>
2020-07-05 22:43:20 +0300

    Updates

---
+++ precache-manifest.0531e48a2f690fb27a0aecb6e439717e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8d0d776d60fbcf32968d1838eaf5d6",
+    "url": "/sample-pdp/index.html"
+  },
+  {
+    "revision": "062b0c2ab6e60982bd82",
+    "url": "/sample-pdp/static/css/main.04612f48.chunk.css"
+  },
+  {
+    "revision": "df890e00691b722a1d74",
+    "url": "/sample-pdp/static/js/2.e0385969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sample-pdp/static/js/2.e0385969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062b0c2ab6e60982bd82",
+    "url": "/sample-pdp/static/js/main.b390290e.chunk.js"
+  },
+  {
+    "revision": "45d211f361f57641b992",
+    "url": "/sample-pdp/static/js/runtime-main.3f3b0402.js"
+  }
+]);
